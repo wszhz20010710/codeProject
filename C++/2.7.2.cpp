3b7fabca3b456adc49dcbd7a21067c7ec2cf4f7c
zhz <1036752554@qq.com>
2023-10-06 16:45:24 +0800

    114514

---
+++ C++/2.7.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double distance;
+    cout << "Please type in distance." << endl;
+    cin >> distance;
+    distance = 220 * distance;
+    cout << "The yard is " << distance << endl;
+    return 0;
+    
+}
