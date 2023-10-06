6b8a60d7d2a4959ab2c3846db41b638078303b60
zhz <desktop@email.com>
2023-10-06 18:50:04 +0800

    zhz

--- C++_primer_plus/Chapter4/4.11.cpp
+++ C++_primer_plus/Chapter4/4.11.cpp
@@ -1,8 +1,34 @@
 //c++中的结构体
 #include <iostream>
+
+using namespace std;
 struct inflatable
 {
     char name[20];
     float volume;
     double price;
 };
+
+int main()
+{
+    inflatable guest = 
+    {
+        "Glorious Gloria", // name
+        1.88,              // volume
+        29.99              // price
+    };
+    
+    inflatable pal = 
+    {
+        "Audacious Arthur",
+        3.12,
+        32.99
+    };
+
+    cout << "Expand your guest list with " << guest.name;
+    cout << " and " << pal.name << "!\n";
+
+    cout << "You can have both for $";
+    cout << guest.price + pal.price << "!\n";
+    return 0;
+}
