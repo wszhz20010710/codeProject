fc6b5d2cb557cedd75ebac4cc3cdbe4930170890
zhz-desktop <1036752554@qq.com>
2024-01-19 14:29:51 +0800

    zhz

--- DeepLearning/Chapter5/5.4.py
+++ DeepLearning/Chapter5/5.4.py
@@ -16,3 +16,20 @@ class CenteredLayer(nn.Module):
 # Y = net(torch.rand(4, 8))
 
 # print(Y.mean())
+
+class MyLinear(nn.Module):
+    def __init__(self, in_units, units):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(in_units, units))
+        self.bias = nn.Parameter(torch.randn(units,))
+        
+    def forward(self, X):
+        linear = torch.matmul(X, self.weight.data) + self.bias.data
+        return F.relu(linear)
+
+linear = MyLinear(5,3)
+print(linear.weight)
+print(linear(torch.rand(2,5)))
+
+net = nn.Sequential(MyLinear(64, 8), MyLinear(8, 1))
+print(net(torch.rand(2, 64)))
