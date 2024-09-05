24c37e151fcc04d114099aaf213ea6eeb41b1e86
zhz <desktop@email.com>
2024-09-05 10:43:51 +0800

    zhz

--- heima_cpp/Class_obj/reload1.cpp
+++ heima_cpp/Class_obj/reload1.cpp
@@ -26,7 +26,20 @@ Person operator+(Person &p1, Person &p2)
     temp.m_A = p1.m_A + p2.m_A;
     temp.m_B = p1.m_B + p2.m_B;
 
+    return temp;
+
 }
+
+// 函数重载的版本
+Person operator+(Person &p, int num)
+{
+    Person temp;
+    temp.m_A = p.m_A + num;
+    temp.m_B = p.m_B + num;
+
+    return temp;
+}
+
 
 void test01()
 {
@@ -40,8 +53,13 @@ void test01()
 
     Person p3 = p1 + p2;
 
+    Person p4 = p1 + 100;
+
     cout << "p3.m_A = " << p3.m_A << endl;
     cout << "p3.m_B = " << p3.m_B << endl;
+
+    cout << "p4.m_A = " << p4.m_A << endl;
+    cout << "p4.m_B = " << p4.m_B << endl;
 }
 
 
